4719297cf5d86b4232dbad9360d217326708533e
proffybean <proffy@comcast.net>
2022-06-23 13:15:46 -0700

    init commit

--- client/src/components/users.jsx
+++ client/src/components/users.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect } from 'react'
+import React, {useState } from 'react'
 import axios from 'axios'
 
 const Users = () => {
-     const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
     const getEmployees = (e) => {
         axios.get('http://localhost:8000/employees')
